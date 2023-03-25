f7e22c9183512ef7f087bc6607faf8be6ab8022b
Arnav Varshney <varshney.arnav@gmail.com>
2023-03-25 18:06:08 +0800

    Implement second order transitions
    Signed-off-by: Arnav Varshney <varshney.arnav@gmail.com>

--- transition.py
+++ transition.py
@@ -7,10 +7,13 @@ class Text:
     text_string = ""
     cleaned_text_string = ""
     length = 0
+
     first_transition_map = {}
     cv1_count = {}
     total_first_order = 0
+
     second_transition_map = {}
+    cv2_count = {}
     total_second_order = 0
 
     def __init__(self, path: str):
@@ -67,6 +70,31 @@ class Text:
         else:
             self.second_transition_map[char] = 1
 
+    def count_cv2(self):
+        for char, nxt, nxt_nxt in self.second_transition_map.keys():
+            if (char, nxt) not in self.cv2_count.keys():
+                val = 0
+                for cv1, cv2, cv3 in self.second_transition_map.keys():
+                    if (cv1, cv2) == (char, nxt):
+                        val += self.second_transition_map[(cv1, cv2, cv3)]
+                self.cv2_count[(char, nxt)] = val
+
+    def write_second_order(self):
+        count_second_order()
+        self.count_cv2()
+        self.total_second_order = sum(self.second_transition_map.values())
+        self.second_transition_map = dict(sorted(self.second_transition_map.items(), key=lambda x: x[1], reverse=True))
+        workbook = xlsxwriter.Workbook('mapped/second_order.xlsx')
+        wk = workbook.add_worksheet('second_order')
+        wk.write_row(0, 0, ('CV1', 'CV2', 'CV3', 'Freq', 'Prob', 'CV1 + CV2 | CV3'))
+        row = 1
+        for each in self.second_transition_map:
+            wk.write_row(row, 0, (each[0], each[1], each[2], self.second_transition_map[each],
+                                  self.second_transition_map[each] / self.total_second_order,
+                                  self.second_transition_map[each] / self.cv2_count[(each[0], each[1])]))
+            row += 1
+        workbook.close()
+
 
 def hi_syllables(word):
     signs = [u'\u0901', u'\u0902', u'\u0903', u'\u093c', u'\u093e', u'\u093f', u'\u0940',
@@ -104,16 +132,16 @@ def count_first_order():
 def count_second_order():
     spl = text.cleaned_text_string.split(" ")
     for i in spl:
-        syll = hi_syllables(i)
-        if len(syll) == 1:
-            text.insert_in_second_map(syll[0])
-        elif len(syll) == 2:
-            text.insert_in_second_map(syll[1])
-        for k in range(len(syll) - 2):
-            curr = syll[k]
-            next = syll[k + 1]
-            next_2 = syll[k + 2]
-            text.insert_in_second_map(curr + '|' + next_2)
+        syllablify = hi_syllables(i)
+        if len(syllablify) == 1:
+            text.insert_in_second_map((syllablify[0], '', ''))
+        elif len(syllablify) == 2:
+            text.insert_in_second_map((syllablify[0], syllablify[1], ''))
+        for k in range(len(syllablify) - 2):
+            curr_char = syllablify[k]
+            next_char = syllablify[k + 1]
+            next_next_char = syllablify[k + 2]
+            text.insert_in_second_map((curr_char, next_char, next_next_char))
 
 
 if __name__ == '__main__':
@@ -122,4 +150,5 @@ if __name__ == '__main__':
     aksharas.update({'ं': Akshara('ं', '902', '', 'anusvar', True)})
     text = Text('hi')
     text.write_first_order()
+    text.write_second_order()
     # print(hi_syllables('उसी'))
