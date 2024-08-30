7b2174a057cf2c0ee889a9f0171e7e3d7109a83b
Arnav Varshney <varshney.arnav@gmail.com>
2024-08-30 19:11:55 +0800

    cleaned corpus analysis

--- transition.py
+++ transition.py
@@ -99,83 +99,83 @@ class Text:
         plt.show()
 
 
-def insert_in_second_map(self, char):
-    if char in self.second_transition_map:
-        self.second_transition_map[char] += 1
-    else:
-        self.second_transition_map[char] = 1
+    def insert_in_second_map(self, char):
+        if char in self.second_transition_map:
+            self.second_transition_map[char] += 1
+        else:
+            self.second_transition_map[char] = 1
 
 
-def count_cv2(self):
-    for char, nxt, nxt_nxt in self.second_transition_map.keys():
-        if (char, nxt) not in self.cv2_count.keys():
-            val = 0
-            for cv1, cv2, cv3 in self.second_transition_map.keys():
-                if (cv1, cv2) == (char, nxt):
-                    val += self.second_transition_map[(cv1, cv2, cv3)]
-            self.cv2_count[(char, nxt)] = val
+    def count_cv2(self):
+        for char, nxt, nxt_nxt in self.second_transition_map.keys():
+            if (char, nxt) not in self.cv2_count.keys():
+                val = 0
+                for cv1, cv2, cv3 in self.second_transition_map.keys():
+                    if (cv1, cv2) == (char, nxt):
+                        val += self.second_transition_map[(cv1, cv2, cv3)]
+                self.cv2_count[(char, nxt)] = val
 
 
-def sort_second_order(self):
-    sorted_dict = {}
-    count_second_order()
-    self.count_cv2()
-    self.second_transition_map = dict(sorted(self.second_transition_map.items(), key=lambda x: x[1], reverse=True))
-    self.total_second_order = sum(self.second_transition_map.values())
-    for pair in self.cv2_count.keys():
-        sorted_dict[pair] = []
-        for each in self.second_transition_map.keys():
-            if pair == (each[0], each[1]):
-                if each not in sorted_dict[pair]:
-                    sorted_dict[pair].append(each)
-    workbook = xlsxwriter.Workbook("mapped/second_order_filtered_kn.xlsx")
-    wk = workbook.add_worksheet("second_order")
-    wk.write_row(0, 0, ("CV1", "CV2", "CV3", "Freq", "Prob", "CV1 + CV2 | CV3"))
-    row = 1
-    for key in sorted_dict.keys():
-        ls = sorted_dict[key]
-        if 3 >= len(ls) > 1:
-            for each in ls:
-                wk.write_row(
-                    row,
-                    0,
-                    (
-                        each[0],
-                        each[1],
-                        each[2],
-                        self.second_transition_map[each],
-                        self.second_transition_map[each] / self.total_second_order,
-                        self.second_transition_map[each] / self.cv2_count[(each[0], each[1])],
-                    ),
-                )
-                row += 1
-    workbook.close()
+    def sort_second_order(self):
+        sorted_dict = {}
+        count_second_order()
+        self.count_cv2()
+        self.second_transition_map = dict(sorted(self.second_transition_map.items(), key=lambda x: x[1], reverse=True))
+        self.total_second_order = sum(self.second_transition_map.values())
+        for pair in self.cv2_count.keys():
+            sorted_dict[pair] = []
+            for each in self.second_transition_map.keys():
+                if pair == (each[0], each[1]):
+                    if each not in sorted_dict[pair]:
+                        sorted_dict[pair].append(each)
+        workbook = xlsxwriter.Workbook("mapped/second_order_filtered_kn.xlsx")
+        wk = workbook.add_worksheet("second_order")
+        wk.write_row(0, 0, ("CV1", "CV2", "CV3", "Freq", "Prob", "CV1 + CV2 | CV3"))
+        row = 1
+        for key in sorted_dict.keys():
+            ls = sorted_dict[key]
+            if 3 >= len(ls) > 1:
+                for each in ls:
+                    wk.write_row(
+                        row,
+                        0,
+                        (
+                            each[0],
+                            each[1],
+                            each[2],
+                            self.second_transition_map[each],
+                            self.second_transition_map[each] / self.total_second_order,
+                            self.second_transition_map[each] / self.cv2_count[(each[0], each[1])],
+                        ),
+                    )
+                    row += 1
+        workbook.close()
 
 
-def write_second_order(self):
-    count_second_order()
-    self.count_cv2()
-    self.total_second_order = sum(self.second_transition_map.values())
-    self.second_transition_map = dict(sorted(self.second_transition_map.items(), key=lambda x: x[1], reverse=True))
-    workbook = xlsxwriter.Workbook("mapped/second_order_kn.xlsx")
-    wk = workbook.add_worksheet("second_order")
-    wk.write_row(0, 0, ("CV1", "CV2", "CV3", "Freq", "Prob", "CV1 + CV2 | CV3"))
-    row = 1
-    for each in self.second_transition_map:
-        wk.write_row(
-            row,
-            0,
-            (
-                each[0],
-                each[1],
-                each[2],
-                self.second_transition_map[each],
-                self.second_transition_map[each] / self.total_second_order,
-                self.second_transition_map[each] / self.cv2_count[(each[0], each[1])],
-            ),
-        )
-        row += 1
-    workbook.close()
+    def write_second_order(self):
+        count_second_order()
+        self.count_cv2()
+        self.total_second_order = sum(self.second_transition_map.values())
+        self.second_transition_map = dict(sorted(self.second_transition_map.items(), key=lambda x: x[1], reverse=True))
+        workbook = xlsxwriter.Workbook("mapped/second_order_kn.xlsx")
+        wk = workbook.add_worksheet("second_order")
+        wk.write_row(0, 0, ("CV1", "CV2", "CV3", "Freq", "Prob", "CV1 + CV2 | CV3"))
+        row = 1
+        for each in self.second_transition_map:
+            wk.write_row(
+                row,
+                0,
+                (
+                    each[0],
+                    each[1],
+                    each[2],
+                    self.second_transition_map[each],
+                    self.second_transition_map[each] / self.total_second_order,
+                    self.second_transition_map[each] / self.cv2_count[(each[0], each[1])],
+                ),
+            )
+            row += 1
+        workbook.close()
 
 
 def count_first_order():
@@ -263,10 +263,13 @@ def remove_tangled_words():
 if __name__ == "__main__":
     # freq_analysis('final Kannada corpus text.11.06.23')
 
-    text = Text("final Kannada corpus - uncleaned")
-    remove_tangled_words()
+    text = Text("final Kannada corpus - cleaned")
+    # remove_tangled_words()
 
-    # text.plot_first_order()  # text.write_first_order()  # text.write_second_order()
+    # text.plot_first_order()
+    freq_analysis("final Kannada corpus - cleaned")
+    text.write_first_order()
+    text.write_second_order()
 
 # plotting freq column * freq(cv1)
 # python - plotting freq column per cv1
